8301aacd70ceb1fbd5c09c96096d7c3b68902bab
Christian Gebhard <christian.gebhard@posteo.de>
2024-02-10 23:00:03 +0100

    Minor refinements, removing unneccessary columns.

--- some-kind-of-cleaning.R
+++ some-kind-of-cleaning.R
@@ -14,7 +14,8 @@ VOY_text <- VOY |>
   unnest(text) |> 
   filter(str_detect(line, pattern = "some (kind|sort|form|type)")) |> 
   mutate(
-    what = str_extract(line, pattern = "some (kind|sort|form|type)")
-  )
+    what_said = str_extract(line, pattern = "some (kind|sort|form|type)")
+  ) |> 
+  select(-c(perspective,setting,description, line))
 
-write_csv(VOY_text, "some_kind_of.csv")
+write_csv(VOY_text, "some_kind_of-data.csv")
