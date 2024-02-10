f4342a8c3f50959d6d31c1bb0f3dd3f38e89d51d
Christian Gebhard <christian.gebhard@posteo.de>
2024-02-11 00:36:15 +0100

    Moves EDA to separate file.

--- some-kind-of-dataviz.R
+++ some-kind-of-dataviz.R
@@ -10,13 +10,3 @@ for(i in seq_along(fonts)) font_add(fonts[i], path[i])
 font_families()
 showtext_auto()
 
-# read data cleaned in some-kind-of-cleaning.R ----
-
-some_kind_of_df <- read_csv("some_kind_of-data.csv")
-
-# exploratory analysis ----
-
-some_kind_of_df |> 
-  group_by(character) |> 
-  count(what_said, sort = T) |> 
-  pivot_wider(id_cols = c(character), names_from = what_said, values_from = n)
